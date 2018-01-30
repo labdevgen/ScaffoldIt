b8cbde9365b004499f752212cf8ef7d9f8fc057d
labdevgen <zakazserov@gmail.com>
2018-01-30 16:59:38 +0700

    Handle loadAssembly Errors
    Handle loadAssembly Errors

---
+++ Deploy.cmd
@@ -0,0 +1,2 @@
+C:\Python35-32\Scripts\pyinstaller.exe --onefile --windowed D:\Sync\Lab\Projects\GenomeAssemblyGUI\ScaffoldIt.py
+cp C:\Python35-32\Scripts\dist\ScaffoldIt.exe D:\Sync\Lab\Projects\GenomeAssemblyGUI\ScaffoldIt
